1ce0dbf5c574dee6c065e937461e3923121e65fb
jk <jozef.kosik@gmail.com>
2023-05-19 16:21:02 +0200

    Add the Startup class

--- Program.cs
+++ Program.cs
@@ -1,27 +1,32 @@
-var builder = WebApplication.CreateBuilder(args);
+var builder = Host.CreateDefaultBuilder(args);
+builder.ConfigureWebHostDefaults(wb =>
+{
+    wb.UseStartup<Startup>();
+});
 
 // Add services to the container.
 
-builder.Services.AddControllersWithViews();
+
+//builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+//// Configure the HTTP request pipeline.
+//if (!app.Environment.IsDevelopment())
+//{
+//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//    app.UseHsts();
+//}
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-app.UseRouting();
+//app.UseHttpsRedirection();
+//app.UseStaticFiles();
+//app.UseRouting();
 
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller}/{action=Index}/{id?}");
+//app.MapControllerRoute(
+//    name: "default",
+//    pattern: "{controller}/{action=Index}/{id?}");
 
-app.MapFallbackToFile("index.html");
+//app.MapFallbackToFile("index.html");
 
 app.Run();
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        // Add services to the container here
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        // Configure the app's request pipeline here
+    }
+}
+
