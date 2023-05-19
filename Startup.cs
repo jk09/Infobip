6445d63f21d57ffb73ce1f6ca4bf53ffb01465c7
jk <jozef.kosik@gmail.com>
2023-05-19 17:05:24 +0200

    Config in the Startup class

--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,6 @@
-﻿public class Startup
+﻿using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+
+public class Startup
 {
     public void ConfigureServices(IServiceCollection services)
     {
@@ -11,6 +13,18 @@
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
         // Configure the app's request pipeline here
+        app.UseStaticFiles();
+        app.UseSpaStaticFiles();
+        app.UseRouting();
+        app.UseEndpoints(endp => { /* configure endpoints */});
+        app.UseSpa(spa => {
+
+            spa.Options.SourcePath = "ClientApp";
+            if (env.IsDevelopment())
+            {
+                spa.UseReactDevelopmentServer(npmScript: "start");
+            }
+        });
     }
 }
 
