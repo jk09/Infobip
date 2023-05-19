626f0b4fad873a78706a6e66d1b51cf64edd0b90
jk <jozef.kosik@gmail.com>
2023-05-19 16:33:44 +0200

    Add SPA extensions packages

--- Startup.cs
+++ Startup.cs
@@ -3,6 +3,8 @@
     public void ConfigureServices(IServiceCollection services)
     {
         // Add services to the container here
+        services.AddControllersWithViews();
+        services.AddSpaStaticFiles(cfg => cfg.RootPath = "ClientApp/build");
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
