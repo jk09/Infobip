e6806bea8ed74faf163eea5796fa8e9cc7feb80d
jk <jozef.kosik@gmail.com>
2023-05-19 17:33:00 +0200

    Add model

---
+++ Models/Carpool.cs
@@ -0,0 +1,12 @@
+﻿namespace Infobip.Models
+{
+    public class Carpool
+    {
+
+        public string? Name { get; set; }
+        public string? CarType { get; set; }
+        public string? Color { get; set; }
+        public string? Plate { get; set; }
+        public int? NumberSeats { get; set; }
+    }
+}
