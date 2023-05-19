b2ae9c8751c35f9090fd64d1d0e96107a6d87e65
jk <jozef.kosik@gmail.com>
2023-05-19 19:52:08 +0200

    Extract file

---
+++ Services/CarpoolRepository.cs
@@ -0,0 +1,29 @@
+﻿using Infobip.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infobip.Services
+{
+    public class CarpoolRepository
+    {
+        private readonly CarpoolDbContext _dbContext;
+
+        public CarpoolRepository(CarpoolDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<Car>> GetAvailableCars()
+        {
+
+            var queryCarsLeftJoinPlans = from car in _dbContext.Set<Car>()
+                                         join plan in _dbContext.Set<TravelPlan>()
+                                             on car.Id equals plan.Id into grouping
+                                         from grp in grouping.DefaultIfEmpty()
+                                         where grp != null
+                                         select car;
+
+
+            return await queryCarsLeftJoinPlans.ToListAsync();
+        }
+    }
+}
