b310074103018cf7b7c0cbdd4c7df13b02e45ca7
jk <jozef.kosik@gmail.com>
2023-05-19 20:13:46 +0200

    edit travel plan

--- Services/CarpoolRepository.cs
+++ Services/CarpoolRepository.cs
@@ -5,22 +5,44 @@ namespace Infobip.Services
 {
     public class CarpoolRepository
     {
-        private readonly CarpoolDbContext _dbContext;
+        private readonly CarpoolDbContext _db;
 
-        public CarpoolRepository(CarpoolDbContext dbContext)
+        public CarpoolRepository(CarpoolDbContext db)
         {
-            _dbContext = dbContext;
+            _db = db;
         }
 
         public async Task<IEnumerable<Car>> GetAvailableCars()
         {
 
-            var queryCarsLeftJoinPlans = from car in _dbContext.Set<Car>()
-                                         join plan in _dbContext.Set<TravelPlan>() on car.Id equals plan.Id 
+            var queryCarsLeftJoinPlans = from car in _db.Cars
+                                         join plan in _db.TravelPlans on car.Id equals plan.Id 
                                          select car;
 
 
             return await queryCarsLeftJoinPlans.ToListAsync();
         }
+
+        public async Task AddNewTravelPlan(TravelPlan plan)
+        {
+            _db.TravelPlans.Add(plan);
+
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task DeleteTravelPlan(TravelPlan plan)
+        {
+            _db.TravelPlans.Remove(plan);
+
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task EditTravelPlan(TravelPlan plan)
+        {
+            _db.TravelPlans.Attach(plan);
+            _db.Entry(plan).State = EntityState.Modified;
+            await _db.SaveChangesAsync();
+        }
+        
     }
 }
