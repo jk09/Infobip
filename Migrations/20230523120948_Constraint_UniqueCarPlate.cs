35cdb61923524dfc6a0125273be1864fee0dc19f
jk <jozef.kosik@gmail.com>
2023-05-23 14:14:27 +0200

    Add unique constraint for car plates

---
+++ Migrations/20230523120948_Constraint_UniqueCarPlate.cs
@@ -0,0 +1,322 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infobip.Migrations
+{
+    /// <inheritdoc />
+    public partial class Constraint_UniqueCarPlate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "NumberSeats",
+                value: 6);
+
+            migrationBuilder.UpdateData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "NumberSeats",
+                value: 5);
+
+            migrationBuilder.UpdateData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "NumberSeats",
+                value: 3);
+
+            migrationBuilder.UpdateData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "NumberSeats",
+                value: 6);
+
+            migrationBuilder.UpdateData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "NumberSeats",
+                value: 4);
+
+            migrationBuilder.UpdateData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "NumberSeats",
+                value: 2);
+
+            migrationBuilder.UpdateData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "NumberSeats",
+                value: 5);
+
+            migrationBuilder.UpdateData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "NumberSeats",
+                value: 6);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "IsDriver",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "IsDriver",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "IsDriver",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "IsDriver",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "IsDriver",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "IsDriver",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "IsDriver",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "IsDriver",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 12,
+                column: "IsDriver",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 13,
+                column: "IsDriver",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 16,
+                column: "IsDriver",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 22,
+                column: "IsDriver",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 24,
+                column: "IsDriver",
+                value: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cars_Plate",
+                table: "Cars",
+                column: "Plate",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Cars_Plate",
+                table: "Cars");
+
+            migrationBuilder.UpdateData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "NumberSeats",
+                value: 4);
+
+            migrationBuilder.UpdateData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "NumberSeats",
+                value: 4);
+
+            migrationBuilder.UpdateData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "NumberSeats",
+                value: 4);
+
+            migrationBuilder.UpdateData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "NumberSeats",
+                value: 2);
+
+            migrationBuilder.UpdateData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "NumberSeats",
+                value: 6);
+
+            migrationBuilder.UpdateData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "NumberSeats",
+                value: 3);
+
+            migrationBuilder.UpdateData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "NumberSeats",
+                value: 6);
+
+            migrationBuilder.UpdateData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "NumberSeats",
+                value: 3);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "IsDriver",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "IsDriver",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "IsDriver",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "IsDriver",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "IsDriver",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "IsDriver",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "IsDriver",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "IsDriver",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 12,
+                column: "IsDriver",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 13,
+                column: "IsDriver",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 16,
+                column: "IsDriver",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 22,
+                column: "IsDriver",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 24,
+                column: "IsDriver",
+                value: false);
+        }
+    }
+}
--- Models/Car.cs
+++ Models/Car.cs
@@ -1,11 +1,15 @@
-﻿namespace Infobip.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Infobip.Models
 {
+    [Index(nameof(Car.Plate), IsUnique = true)]
     public class Car
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string CarType { get; set; }
         public string Color { get; set; }
+        
         public string Plate { get; set; }
         public int NumberSeats { get; set; }
     }
